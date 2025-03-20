a92166d8428f9f79d1dc5baddd4f9baa2f11332c
Stas <staserdotesku333@gmail.com>
2025-03-20 22:29:06 +0200

    DataUpdate

--- FitnessCenter.Data/Class.cs
+++ FitnessCenter.Data/Class.cs
@@ -3,4 +3,7 @@
 public class Class : BaseModel
 {
     public string Name { get; set; }
+    public TimeSpan Time { get; set; }
+    public decimal Cost { get; set; }
+    public int MaxNumber { get; set; }
 }
---
+++ FitnessCenter.Data/Reserv.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessCenter.Data;
+
+public class Reserv : BaseModel
+{
+    public int ClassId { get; set; }
+    public Class Class { get; set; }
+    public int TrainerId { get; set; }
+    public Trainer Trainer { get; set; }
+    public string Type { get; set; }
+    public int Number { get; set; }
+}
